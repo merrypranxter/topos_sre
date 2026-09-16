cb2a24f807768f9cba44f9c10d0a9b85323a5f93
merry <159569959+merrypranxter@users.noreply.github.com>
2026-09-16 14:07:12 -0500

    Remove UI utility dependencies

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,5 @@
-import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
+type ClassName = string | false | null | undefined;
 
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+export function cn(...inputs: ClassName[]) {
+  return inputs.filter(Boolean).join(" ");
 }
